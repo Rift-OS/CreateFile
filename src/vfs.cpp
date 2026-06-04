3e5262210c0a52740b8a6f9046424228a66c4989
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-06-04 17:28:52 +0900

    Update vfs.cpp

--- src/vfs.cpp
+++ src/vfs.cpp
@@ -3,11 +3,10 @@
 namespace RiftOS {
 
 VirtualFileSystem::VirtualFileSystem() {
-    // コンストラクタでテーブルが初期化されるため明示的な処理は不要
+    // テーブルは構造体のコンストラクタによって自動初期化されます
 }
 
 int VirtualFileSystem::CreateFile(const char* path, FileAttribute attr) {
-    // 1. 空いているファイルハンドルを検索
     int fd = -1;
     for (size_t i = 0; i < MAX_OPEN_FILES; ++i) {
         if (!file_table[i].is_opened) {
@@ -17,17 +16,15 @@ int VirtualFileSystem::CreateFile(const char* path, FileAttribute attr) {
     }
 
     if (fd == -1) {
-        return -1; // 空きハンドルなし
+        return -1; 
     }
 
-    // 2. ファイル名のコピー（安全なループ処理）
     size_t j = 0;
     for (j = 0; j < 11 && path[j] != '\0'; ++j) {
         file_table[fd].filename[j] = path[j];
     }
     file_table[fd].filename[j] = '\0';
 
-    // 3. メタデータの設定
     file_table[fd].attr = attr;
     file_table[fd].file_size = 0;
     file_table[fd].start_cluster = 0;
